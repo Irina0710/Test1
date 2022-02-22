b94f51f3c3eb7c32fb2681d219355f58aa0b30ad
Irina0710 <defmail2me@gmail.com>
2022-02-22 16:12:58 +0300

    Code aded

---
+++ Code/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string[] array = {"geek", "brains", "22", "hello", "c#", "World", ":)"};
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"\"{array[i]}\"");
+    }
+}
+
+PrintArray(array);
+Console.WriteLine(" => ");
+var result = new string[array.Length];  
+var temp = 0; 
+
+foreach (var value in array)  
+{
+    if (value.Length <= 3)   
+    {
+        result[temp] = value; 
+        temp++; 
+    }
+}
+
+Console.Write(string.Join(Environment.NewLine, result, 0, temp));
+
